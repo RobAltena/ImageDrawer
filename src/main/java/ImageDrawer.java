ab9c4e534fdee50ebfb3edc6802201e7eecd7094
Robert Altena <Rob@Ra-ai.com>
2020-03-18 18:59:15 +0900

    wip
    Signed-off-by: Robert Altena <Rob@Ra-ai.com>

--- src/main/java/ImageDrawer.java
+++ src/main/java/ImageDrawer.java
@@ -1,4 +1,5 @@
 
+import org.datavec.image.loader.Java2DNativeImageLoader;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -7,9 +8,12 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.conditions.Conditions;
 import org.nd4j.linalg.learning.config.Adam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
@@ -26,13 +30,16 @@ public class ImageDrawer {
     private MultiLayerNetwork nn; // The neural network.
 
     private BufferedImage originalImage;
-    private BufferedImage generatedImage;
+    private JLabel generatedLabel;
+
     private int[] pixels;          // array of pixels of original image.
-    private int[] pixelsGenerated; // array of pixels to generate image.
+    private INDArray xPixels; // x coordinates of the pixels for the NN.
+    private INDArray yPixels; // y coordinates of the pixels for the NN.
 
     private INDArray xyOut; //x,y grid to calculate the output image. Needs to be calculated once, then re-used.
 
     private Random r = new Random();
+    Java2DNativeImageLoader j2dNil;
 
 
     private void init() throws IOException {
@@ -41,17 +48,16 @@ public class ImageDrawer {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         originalImage = ImageIO.read(getClass().getResource("Mona_Lisa_2.png"));
-        generatedImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), originalImage.getType());
+        //start with a blank image of the same size as the original.
+        BufferedImage generatedImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), originalImage.getType());
 
         int width = originalImage.getWidth();
         int height = originalImage.getHeight();
         pixels = originalImage.getRGB(0, 0, width, height, null, 0, width);
-        pixelsGenerated = new int[pixels.length];
-        generatedImage.setRGB(0, 0, width, height, pixels, 0, width);
 
         final JLabel originalLabel = new JLabel(new ImageIcon(originalImage));
-        final JLabel generatedLabel = new JLabel(new ImageIcon(generatedImage
-        ));
+        generatedLabel = new JLabel(new ImageIcon(generatedImage));
+
         originalLabel.setBounds(0,0, width, height);
         generatedLabel.setBounds(width, 0, width, height);//x axis, y axis, width, height
 
@@ -62,6 +68,7 @@ public class ImageDrawer {
         mainFrame.setLayout(null);//using no layout managers
         mainFrame.setVisible(true);//making the frame visible
 
+        j2dNil = new Java2DNativeImageLoader();
         nn = createNN();
         xyOut = calcGrid();
 
@@ -78,9 +85,9 @@ public class ImageDrawer {
      */
     private static MultiLayerNetwork createNN() {
         int seed = 2345;
-        double learningRate = 0.01;
+        double learningRate = 0.001;
         int numInputs = 2;   // x and y.
-        int numHiddenNodes = 500;
+        int numHiddenNodes = 1000;
         int numOutputs = 3 ; //R, G and B value.
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
@@ -119,8 +126,8 @@ public class ImageDrawer {
      * Training the NN and updating the current graphical output.
      */
     private void onCalc(){
-        int batchSize = 3000;
-        int numBatches = 100;
+        int batchSize = 10000;
+        int numBatches = 10;
         for (int i =0; i< numBatches; i++){
             DataSet ds = generateDataSet(batchSize);
             nn.fit(ds);
@@ -146,10 +153,13 @@ public class ImageDrawer {
         INDArray out = Nd4j.zeros(batchSize, 3);
         float [] RGBColorComponents = new float[3];
 
+        INDArray xindex = Nd4j.rand(batchSize).muli(w).castTo(DataType.UINT32);
+        INDArray yindex = Nd4j.rand(batchSize).muli(h).castTo(DataType.UINT32);
+
         for (int index = 0; index < batchSize; index++) {
-            int i = r.nextInt(w);
-            int j = r.nextInt(h);
-            double xp = scaleXY(i,w);
+            int i = xindex.getInt(index);
+            int j = yindex.getInt(index);
+            double xp = xPixels.getDouble(i); //scaleXY(i,w);
             double yp = scaleXY(j,h);
 
             int indexPixel = j * w + i;
@@ -173,23 +183,20 @@ public class ImageDrawer {
         int w = originalImage.getWidth();
         int h = originalImage.getHeight();
 
-        INDArray out = nn.output(xyOut);
+        INDArray out = nn.output(xyOut); // The raw NN output.
+        BooleanIndexing.replaceWhere(out, 0.0, Conditions.lessThan(0.0)); // Cjip between 0 and 1.
+        BooleanIndexing.replaceWhere(out, 1.0, Conditions.greaterThan(1.0));
+        out = out.mul(255).castTo(DataType.BYTE); //convert to bytes.
 
-        for (int i = 0; i < w; i++) {
-            for (int j = 0; j < h; j++) {
-                int index = i + w * j;
-                float red = capNNOutput(out.getFloat(index, 0));
-                float  green = capNNOutput(out.getFloat(index, 1));
-                float  blue = capNNOutput(out.getFloat(index, 2));
+        INDArray r = out.getColumn(0); //Extract the individual color layers.
+        INDArray g = out.getColumn(1);
+        INDArray b = out.getColumn(2);
 
-                Color c = new Color(red, green, blue);
-                pixelsGenerated[index] = c.getRGB();
-            }
-        }
+        INDArray imgArr = Nd4j.vstack(b, g, r).reshape(3, h, w); // recombine the colors and reshape to image size.
 
-        generatedImage.setRGB(0, 0, w, h, pixelsGenerated, 0, w);
+        BufferedImage img = j2dNil.asBufferedImage(imgArr); //update the UI.
+        generatedLabel.setIcon(new ImageIcon(img));
     }
-
 
     /**
      * The x,y grid to calculate the NN output. Only needs to be calculated once.
@@ -197,30 +204,11 @@ public class ImageDrawer {
     private INDArray calcGrid(){
         int w = originalImage.getWidth();
         int h = originalImage.getHeight();
-        int numPoints = h * w;
-        INDArray res = Nd4j.zeros(numPoints, 2);
 
-        //TODO: There is a more elegant way to do this with Nd4j.
-        for (int i = 0; i < w; i++) {
-            double xp = scaleXY(i,w);
-            for (int j = 0; j < h; j++) {
-                int index = i + w * j;
-                double yp = scaleXY(j,h);
-
-                res.put(index, 0, xp); //2 inputs. x and y.
-                res.put(index, 1, yp);
-            }
-        }
-
-        return res;
-    }
-
-    /**
-     * Make sure the color values are >=0 and <=1
-     */
-    private static float capNNOutput(float x) {
-        float tmp = (x<0.0f) ? 0.0f : x;
-        return (tmp > 1.0f) ? 1.0f : tmp;
+        xPixels = Nd4j.linspace(-0.5, 0.5, w, DataType.DOUBLE);
+        yPixels = Nd4j.linspace(-0.5, 0.5, h, DataType.DOUBLE);
+        INDArray [] mesh = Nd4j.meshgrid(xPixels, yPixels);
+        return Nd4j.vstack(mesh[0].ravel(), mesh[1].ravel()).transpose();
     }
 
     /**
